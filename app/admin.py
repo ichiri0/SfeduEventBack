29b44de8a4d9e45de1f93aac64b4087db1647df0
ichiri0 <quarkkkk@email.com>
2023-12-23 13:47:44 +0300

    Регистрация моделей в Админ-панели

---
+++ app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from app.models import Departament, Direction, Group, Institute, GroupCourse, Role, UserProfile, Event
+# Register your models here.
+
+
+admin.site.register(Departament)
+admin.site.register(Direction)
+admin.site.register(Group)
+admin.site.register(Institute)
+admin.site.register(GroupCourse)
+admin.site.register(Role)
+admin.site.register(UserProfile)
+admin.site.register(Event)
