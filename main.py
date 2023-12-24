34ac73e0f52cd768eaf18ee9673e1b06a61cb6c2
ichiri0 <quarkkkk@email.com>
2023-12-24 17:45:14 +0300

    модификая main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from colorama import Fore, init
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes.user import router as user_router
 from routes.institute import router as insitute_router
 from routes.event import router as event_router
@@ -19,8 +19,25 @@ app.include_router(event_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Замените на нужный адрес React приложения
+    allow_origins=[
+        "http://localhost:3000",
+        "https://epic-dove-pretty.ngrok-free.app/",
+        "http://10.131.57.108:3000",
+                   ],  # Замените на нужный адрес React приложения
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.middleware("http")
+async def add_custom_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["User-Agent"] = "69420"  # Установка вашего заголовка
+    return response
+
+
+@app.middleware("https")
+async def add_custom_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["User-Agent"] = "69420"  # Установка вашего заголовка
+    return response
